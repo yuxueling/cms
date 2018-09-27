f6d62ee382ffe5b04bbf260fb0802d51dc2877c1
Hzof <Hzof@DESKTOP-TVVV7ID>
2018-09-27 18:11:46 +0800

    完美解决了无法显示的问题

--- hnjx/src/main/java/com/cloudht/system/controller/MianController.java
+++ hnjx/src/main/java/com/cloudht/system/controller/MianController.java
@@ -25,8 +25,11 @@ import java.util.List;
 @Controller
 public class MianController extends BaseController {
 	@Autowired MenuService menuService;
-	
-	@GetMapping({"","/index","/index.html","/xmx/index"})
+	@GetMapping({"","/","index","index.html"})
+	public String redirect() {
+		return "redirect:/xmx/index";
+	}
+	@GetMapping({"/xmx/index"})
 	public String index() {
 		return "/xmx/index";
 	}
