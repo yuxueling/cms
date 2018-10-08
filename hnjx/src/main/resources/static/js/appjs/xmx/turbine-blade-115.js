892ed80865db2a7109c3eaa84aa4e27993a62e8e
yuxueling <yuxueling08@163.com>
2018-10-08 16:22:46 +0800

    index

---
+++ hnjx/src/main/resources/static/js/appjs/xmx/turbine-blade-115.js
@@ -0,0 +1,54 @@
+
+$(function(){
+    vm.init();
+});
+
+var vm = new Vue({
+    el: '#site',
+    data: {
+        categoryTree:{},
+        productList:[],
+        limit:100,
+        offset:0,
+        contCategoryId:0,
+        langType:'english',
+        categoryName:''
+    },
+    methods: {
+        init: function () {
+
+            $.ajax({
+                url: "/cont/contCategory/treeInfo",
+                type: "post",
+                data: {
+                    langType: vm.langType
+                },
+                success: function (data) {
+                    vm.categoryTree=data;
+                    var contCategoryId = data.children[0].id;
+                    var categoryName = data.children[0].text;
+                    //初始化产品
+                    vm.listProduct(contCategoryId,categoryName);
+                }
+            });
+
+        } ,
+        listProduct: function (contCategoryId,categoryName) {
+            vm.categoryName=categoryName;
+            $.ajax({
+                url: "/contXmx/listProductByCategory",
+                type: "post",
+                data: {
+                    limit:vm.limit,
+                    offset:vm.offset,
+                    contCategoryId:contCategoryId,
+                    langType:vm.langType
+                },
+                success: function (data) {
+                    vm.productList=data.rows;
+                }
+            });
+        }
+
+    }
+});
